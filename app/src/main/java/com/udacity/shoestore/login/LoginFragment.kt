93f3abedc3f4d9457603ba62460009e0e69dbca4
Ahmed Matem Ahmed <ahmedmatem@gmail.com>
2021-03-27 08:01:34 +0000

    Add LoginFragment

---
+++ app/src/main/java/com/udacity/shoestore/login/LoginFragment.kt
@@ -0,0 +1,30 @@
+package com.udacity.shoestore.login
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.udacity.shoestore.R
+import com.udacity.shoestore.databinding.FragmentLoginBinding
+
+class LoginFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val binding: FragmentLoginBinding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
+        return binding.root
+    }
+}
